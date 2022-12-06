e308cadf46d481e86b7f5edca50a09af5a83d783
Mohammed Hamdan <mh2022ets@gmail.com>
2022-12-05 20:13:32 -0500

    Add files via upload

---
+++ Deep_Dive_Python3/Part4/Descriptors/name_main.py
@@ -0,0 +1,23 @@
+
+# import threading
+# import time 
+
+
+
+
+# print(threading.active_count())
+# print(threading.enumerate())
+
+
+# def eat():
+#     print('eat')
+
+#     time.sleep(2)
+    
+# def drink():
+#     print('drink')
+#     time.sleep(2)
+    
+# def study():
+#     print('study')
+#     time.sleep(2)
